0de90f86e25e0c8477abc9f1eab95054aa16b751
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 <kishkoblu@mail.ru>
2024-03-24 18:37:45 +0300

    slider-stages

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 'use strict';
+//слайдер stages
+const btnStageNext = document.querySelector('.btn-controls.next'),
+      btnStagePrev = document.querySelector('.btn-controls.prev'),
+      paginationItems = document.querySelectorAll('.pagination__item'),     
+      slider = document.querySelector('.stages-slider__list');
+let transformValues = 0,
+    counter = 0,
+    numsOfSlides =  paginationItems.length,
+    widthSlide = 100,
+    gapValue = 10;
+
+btnStageNext.addEventListener('click',()=>{
+    console.log(window.innerWidth)
+    if(window.innerWidth >= 768){
+        widthSlide = 50;
+    }
+        btnStagePrev.disabled = false;
+        paginationItems[counter].classList.remove('pagination__item_active');
+        counter += 1;
+        paginationItems[counter].classList.add('pagination__item_active');
+    transformValues -= widthSlide;
+    slider.style.transform = `translateX(calc(${transformValues}% - ${gapValue * (counter + 1)}px))`
+    if(counter === (numsOfSlides - 1)) {
+        btnStageNext.disabled = true;
+    }
+})
+
+btnStagePrev.addEventListener('click',()=>{
+    console.log(window.innerWidth)
+    if(window.innerWidth >= 768){
+        widthSlide = 50;
+    }
+    btnStageNext.disabled = false;
+    paginationItems[counter].classList.remove('pagination__item_active');
+            counter -= 1;
+    paginationItems[counter].classList.add('pagination__item_active');
+    transformValues += widthSlide;
+    slider.style.transform = `translateX(calc(${transformValues}% - ${gapValue * (counter + 1)}px))`
+        if(counter === 0) {
+        btnStagePrev.disabled = true;
+    }
+})
+
+window.addEventListener('resize', function(event) {
+    console.log(window.innerWidth);
+    widthSlide = 100;
+    transformValues = 0;
+    slider.style.transform = `translateX(${transformValues}px)`;
+    paginationItems[counter].classList.remove('pagination__item_active');
+    counter = 0;
+    paginationItems[counter].classList.add('pagination__item_active');
+    btnStageNext.disabled = false;
+    btnStagePrev.disabled = true;
+}, true);
